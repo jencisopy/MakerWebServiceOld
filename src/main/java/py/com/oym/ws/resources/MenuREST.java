49bc5a4ba5732ef4cb2734aadf2496cfbec1acde
oymdev04 <oymdev4@oym.com.py>
2018-10-22 11:49:48 -0300

    MKMV-83 WebServices Generación de Menú (Maker Gestion - Inmobiliaria)

--- src/main/java/py/com/oym/ws/resources/MenuREST.java
+++ src/main/java/py/com/oym/ws/resources/MenuREST.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package py.com.oym.ws.resources;
 
 import javax.ejb.Stateless;
@@ -11,26 +6,22 @@ import javax.ws.rs.OPTIONS;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
-/**
- *
- * @author alberto
- */
 @Stateless
 @Path("menu")
 public class MenuREST {
 
     @OPTIONS
-    @Path("menugenerado")
+    @Path("menugestion")
     @Produces({"application/json"})
-    public String menugenerado() {
+    public String menugestion1() {
         return "";
     }
 
     @GET
-    @Path("menugenerado")
+    @Path("menugestion")
     @Produces({"application/json"})
-    public String menu() {
-        String menu = "[{\"title\":\"Inicio\"," + "\"component\":\"InicioPage\"," + "\"icon\":\"Home\"},"
+    public String menugestion() {
+        String menugestion = "[{\"title\":\"Inicio\"," + "\"component\":\"InicioPage\"," + "\"icon\":\"Home\"},"
                 + "{\"title\":\"Productos\"," + "\"component\":\"ProductosListaPage\"," + "\"icon\":\"basket\"},"
                 + "{\"title\":\"Clientes\"," + "\"component\":\"ClientesListaPage\"," + "\"icon\":\"person\"},"
                 + "{\"title\":\"Proveedores\"," + "\"component\":\"ProveedoresListaPage\"," + "\"icon\":\"cart\"},"
@@ -39,19 +30,19 @@ public class MenuREST {
                 + "{\"title\":\"Salidas\"," + "\"component\":\"StockSalidasListaPage\"," + "\"icon\":\"cash\"},"
                 + "{\"title\":\"Envios\"," + "\"component\":\"StockEnviosListaPage\"," + "\"icon\":\"cash\"}"
                 + "]}, "
-                  + "{\"title\":\"Compras\"," + "\"icon\":\"basket\"," + ""
+                + "{\"title\":\"Compras\"," + "\"icon\":\"basket\"," + ""
                 + "\"submenu\":[{\"title\":\"Ordenes\"," + "\"component\":\"ComprasOrdenesListaPage\"," + "\"icon\":\"document\"},"
                 + "{\"title\":\"Compras\"," + "\"component\":\"ComprasListaPage\"," + "\"icon\":\"cart\"},"
                 + "{\"title\":\"Créditos\"," + "\"component\":\"ComprasCreditosListaPage\"," + "\"icon\":\"card\"},"
                 + "{\"title\":\"Débitos\"," + "\"component\":\"ComprasDebitosListaPage\"," + "\"icon\":\"card\"}"
                 + "]}, "
-                  + "{\"title\":\"Ventas\"," + "\"icon\":\"cash\"," + ""
+                + "{\"title\":\"Ventas\"," + "\"icon\":\"cash\"," + ""
                 + "\"submenu\":[{\"title\":\"Pedidos\"," + "\"component\":\"VentasPedidosListaPage\"," + "\"icon\":\"cash\"},"
                 + "{\"title\":\"Facturas\"," + "\"component\":\"VentasFacturasListaPage\"," + "\"icon\":\"document\"},"
                 + "{\"title\":\"Créditos\"," + "\"component\":\"VentasCreditosListaPage\"," + "\"icon\":\"card\"},"
                 + "{\"title\":\"Débitos\"," + "\"component\":\"VentasDebitosListaPage\"," + "\"icon\":\"card\"}"
                 + "]}, "
-                 + "{\"title\":\"Finanzas\"," + "\"icon\":\"stats\"," + ""
+                + "{\"title\":\"Finanzas\"," + "\"icon\":\"stats\"," + ""
                 + "\"submenu\":[{\"title\":\"Pagos\"," + "\"component\":\"FinanzasPagosListaPage\"," + "\"icon\":\"cash\"},"
                 + "{\"title\":\"Cobranzas\"," + "\"component\":\"FinanzasCobrosListaPage\"," + "\"icon\":\"cash\"}"
                 + "]}, "
@@ -59,7 +50,30 @@ public class MenuREST {
                 + "{\"title\":\"Acerca de\"," + "\"component\":\"AcercaDePage\"," + "\"icon\":\"paper\"}"
                 + "]";
 
-        return menu;
+        return menugestion;
+    }
+
+    @OPTIONS
+    @Path("menuinmobiliario")
+    @Produces({"application/json"})
+    public String menuinmobiliario1() {
+        return "";
+    }
+
+    @GET
+    @Path("menuinmobiliario")
+    @Produces({"application/json"})
+    public String menuinmobiliario() {
+        String menuinmobiliario = "[{\"title\":\"Inicio\"," + "\"component\":\"InicioPage\"," + "\"icon\":\"Home\"},"
+                + "{\"title\":\"Departamentos\"," + "\"component\":\"DepartamentosListaPage\"," + "\"icon\":\"home\"},"
+                + "{\"title\":\"Fracciones\"," + "\"component\":\"FraccionesListaPage\"," + "\"icon\":\"grid\"},"
+                + "{\"title\":\"Clientes\"," + "\"component\":\"ClientesFormPage\"," + "\"icon\":\"person\"},"
+                + "{\"title\":\"Contratos\"," + "\"component\":\"ContratosListaPage\"," + "\"icon\":\"paper\"},"
+                + "{\"title\":\"Configuracion\"," + "\"component\":\"ConfigPage\"," + "\"icon\":\"settings\"},"
+                + "{\"title\":\"Acerca de\"," + "\"component\":\"AcercaDePage\"," + "\"icon\":\"paper\"}"     
+                + "]";
+
+        return menuinmobiliario;
     }
 
 }
