3af434b40c4238f152e87540c275ff76096cb88e
oymdev04 <oymdev4@oym.com.py>
2018-10-24 10:09:41 -0300

    MKMV-83 WebServices Menú Maker Gestión-Inmobiliaria, Ajustes Menores -Comentarios en cada Linea del Metodo para mejor entendimiento.

--- src/main/java/py/com/oym/ws/resources/MenuREST.java
+++ src/main/java/py/com/oym/ws/resources/MenuREST.java
@@ -21,36 +21,36 @@ public class MenuREST {
     @Path("menugestion")
     @Produces({"application/json"})
     public String menugestion() {
-        String menugestion = "[{\"title\":\"Inicio\"," + "\"component\":\"InicioPage\"," + "\"icon\":\"home\"},"
-                + "{\"title\":\"Productos\"," + "\"component\":\"ProductosListaPage\"," + "\"icon\":\"basket\"},"
-                + "{\"title\":\"Clientes\"," + "\"component\":\"ClientesListaPage\"," + "\"icon\":\"person\"},"
-                + "{\"title\":\"Proveedores\"," + "\"component\":\"ProveedoresListaPage\"," + "\"icon\":\"cart\"},"
-                + "{\"title\":\"Stock\"," + "\"icon\":\"cash\"," + ""
+        String menugestion = "[{\"title\":\"Inicio\"," + "\"component\":\"InicioPage\"," + "\"icon\":\"home\"},"//Menú Inicio
+                + "{\"title\":\"Productos\"," + "\"component\":\"ProductosListaPage\"," + "\"icon\":\"basket\"},"//Menú Productos
+                + "{\"title\":\"Clientes\"," + "\"component\":\"ClientesListaPage\"," + "\"icon\":\"person\"},"//Menú Clientes
+                + "{\"title\":\"Proveedores\"," + "\"component\":\"ProveedoresListaPage\"," + "\"icon\":\"cart\"},"//Menu Proveedores
+                + "{\"title\":\"Stock\"," + "\"icon\":\"cash\"," + ""//Menú Stock con Submenus Entrada,Salida y Envio
                 + "\"submenu\":[{\"title\":\"Entradas\"," + "\"component\":\"StockEntradasListaPage\"," + "\"icon\":\"cash\"},"
                 + "{\"title\":\"Salidas\"," + "\"component\":\"StockSalidasListaPage\"," + "\"icon\":\"cash\"},"
                 + "{\"title\":\"Envios\"," + "\"component\":\"StockEnviosListaPage\"," + "\"icon\":\"cash\"}"
-                + "]}, "
+                + "]}, "//Menú Compras con Submenus Compras,Ordenes,Fondo Fijo, Créditos y Débitos
                 + "{\"title\":\"Compras\"," + "\"icon\":\"basket\"," + ""
                 + "\"submenu\":[{\"title\":\"Ordenes\"," + "\"component\":\"ComprasOrdenesListaPage\"," + "\"icon\":\"document\"},"
                 + "{\"title\":\"Compras\"," + "\"component\":\"ComprasListaPage\"," + "\"icon\":\"cart\"},"
                 + "{\"title\":\"Fondo Fijo\"," + "\"component\":\"FondoFijoListaPage\"," + "\"icon\":\"cart\"},"
                 + "{\"title\":\"Créditos\"," + "\"component\":\"ComprasCreditosListaPage\"," + "\"icon\":\"card\"},"
                 + "{\"title\":\"Débitos\"," + "\"component\":\"ComprasDebitosListaPage\"," + "\"icon\":\"card\"}"
-                + "]}, "
+                + "]}, "//Menú Ventas con Submenus Pedidos,Facturas,Créditos y Débitos
                 + "{\"title\":\"Ventas\"," + "\"icon\":\"cash\"," + ""
                 + "\"submenu\":[{\"title\":\"Pedidos\"," + "\"component\":\"VentasPedidosListaPage\"," + "\"icon\":\"cash\"},"
                 + "{\"title\":\"Facturas\"," + "\"component\":\"VentasFacturasListaPage\"," + "\"icon\":\"document\"},"
                 + "{\"title\":\"Créditos\"," + "\"component\":\"VentasCreditosListaPage\"," + "\"icon\":\"card\"},"
                 + "{\"title\":\"Débitos\"," + "\"component\":\"VentasDebitosListaPage\"," + "\"icon\":\"card\"}"
-                + "]}, "
+                + "]}, "//Menú Finanzas con Submenus Pagos,Cobranzas,Ret.Clientes y Ret.Proveedor
                 + "{\"title\":\"Finanzas\"," + "\"icon\":\"stats\"," + ""
                 + "\"submenu\":[{\"title\":\"Pagos\"," + "\"component\":\"FinanzasPagosListaPage\"," + "\"icon\":\"cash\"},"
                 + "{\"title\":\"Cobranzas\"," + "\"component\":\"FinanzasCobrosListaPage\"," + "\"icon\":\"cash\"},"
                 + "{\"title\":\"Ret. Cliente\"," + "\"component\":\"FinanzasRetClienteListaPage\"," + "\"icon\":\"cash\"},"
                 + "{\"title\":\"Ret. Proveedor\"," + "\"component\":\"FinanzasRetProveedorListaPage\"," + "\"icon\":\"cash\"}"
                 + "]}, "
-                + "{\"title\":\"Configuración\"," + "\"component\":\"ConfiguracionMenuPage\"," + "\"icon\":\"build\"},"
-                + "{\"title\":\"Acerca de\"," + "\"component\":\"AcercaDePage\"," + "\"icon\":\"paper\"}"
+                + "{\"title\":\"Configuración\"," + "\"component\":\"ConfiguracionMenuPage\"," + "\"icon\":\"build\"},"//Menú Configuración
+                + "{\"title\":\"Acerca de\"," + "\"component\":\"AcercaDePage\"," + "\"icon\":\"paper\"}"//Menú Acerca De
                 + "]";
 
         return menugestion;
@@ -67,13 +67,13 @@ public class MenuREST {
     @Path("menuinmobiliario")
     @Produces({"application/json"})
     public String menuinmobiliario() {
-        String menuinmobiliario = "[{\"title\":\"Inicio\"," + "\"component\":\"InicioPage\"," + "\"icon\":\"Home\"},"
-                + "{\"title\":\"Departamentos\"," + "\"component\":\"DepartamentosListaPage\"," + "\"icon\":\"home\"},"
-                + "{\"title\":\"Fracciones\"," + "\"component\":\"FraccionesListaPage\"," + "\"icon\":\"grid\"},"
-                + "{\"title\":\"Clientes\"," + "\"component\":\"ClientesFormPage\"," + "\"icon\":\"person\"},"
-                + "{\"title\":\"Contratos\"," + "\"component\":\"ContratosListaPage\"," + "\"icon\":\"paper\"},"
-                + "{\"title\":\"Configuracion\"," + "\"component\":\"ConfigPage\"," + "\"icon\":\"settings\"},"
-                + "{\"title\":\"Acerca de\"," + "\"component\":\"AcercaDePage\"," + "\"icon\":\"paper\"}"     
+        String menuinmobiliario = "[{\"title\":\"Inicio\"," + "\"component\":\"InicioPage\"," + "\"icon\":\"home\"},"//Menú Inicio
+                + "{\"title\":\"Departamentos\"," + "\"component\":\"DepartamentosListaPage\"," + "\"icon\":\"home\"},"//Menú Departamentos
+                + "{\"title\":\"Fracciones\"," + "\"component\":\"FraccionesListaPage\"," + "\"icon\":\"grid\"},"//Menú Fracciones
+                + "{\"title\":\"Clientes\"," + "\"component\":\"ClientesFormPage\"," + "\"icon\":\"person\"},"//Menú Clientes
+                + "{\"title\":\"Contratos\"," + "\"component\":\"ContratosListaPage\"," + "\"icon\":\"paper\"},"//Menú Contrato
+                + "{\"title\":\"Configuracion\"," + "\"component\":\"ConfigPage\"," + "\"icon\":\"settings\"},"//Menú Configuración
+                + "{\"title\":\"Acerca de\"," + "\"component\":\"AcercaDePage\"," + "\"icon\":\"paper\"}"//Menú Acerca De     
                 + "]";
 
         return menuinmobiliario;
